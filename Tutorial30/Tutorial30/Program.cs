1a9988d10a3dce2e72c743d9f9c6e4ee63c5e2f0
Michael Rusch <112953093+MichaelR1974@users.noreply.github.com>
2023-01-26 09:34:52 +0100

    Add files via upload

---
+++ Tutorial30/Tutorial30/Program.cs
@@ -0,0 +1,79 @@
+﻿
+
+
+namespace Tutorial30 {
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matrix = new int[10, 10];
+
+            string input = "";
+            do {
+                Console.Write("Y: ");
+                int posY = Convert.ToInt32(Console.ReadLine().ToUpper()[0] - 'A');
+                Console.Write("X: ");
+                int posX = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                matrix[posX, posY] = 1;
+                // print schachbrett
+                Console.WriteLine();
+                Console.Write("   ");
+                for (int x = 0; x < matrix.GetLength(1); x++)
+                {
+                    Console.Write("  {0,-2}", x + 1);
+                }
+
+                Console.WriteLine();
+                Console.Write("   ");
+                for (int x = 0; x < matrix.GetLength(1); x++)
+                {
+                    Console.Write("+---");
+                }
+                Console.WriteLine("+");
+
+                for (int y = 0; y < matrix.GetLength(0); y++)
+                {
+
+                    Console.Write(" {0} | ", (char)('A' + y));
+
+                    for (int x = 0; x < matrix.GetLength(1); x++)
+                    {
+                        if (matrix[y, x] == 0)
+                        {
+                            Console.Write("  | ");
+                        }
+                        else
+                        {
+                            Console.Write("X | ");
+                        }
+                    }
+                    Console.WriteLine();
+                    Console.Write("   ");
+
+                    for (int x = 0; x < matrix.GetLength(1); x++)
+                    {
+                        Console.Write("+---");
+                    }
+                    Console.WriteLine("+");
+                }
+                    Console.WriteLine();
+                    Console.Write("Go on (y/n)? ");
+                    input = Console.ReadLine();
+                    Console.Clear();
+                
+
+            }while (input != "n") ;
+            Console.WriteLine("....stopped");
+
+    
+
+            
+        }
+
+
+    } 
+}
+    
---
+++ Tutorial31/Tutorial31/Program.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace Tutorial31
+{
+	class Programm
+	{
+		public static void Main(string[] args)
+		{
+			int newNumber = 5;
+            int newNumber1 = 15;
+            newNumber = MyFunktion(newNumber);
+			Console.WriteLine(newNumber);
+
+			newNumber1 = CrazyFunktion(newNumber1);
+			Console.WriteLine(newNumber1);
+
+			PrintSomethink();
+
+			int[] arr1 = new int[] { 10, 20, 40 };
+			int[] arr2 = new int[] { 15, 25, 45 }; ;
+
+			PrintArray(arr1);
+			PrintArray(arr2);
+        }
+			public static int MyFunktion(int number)
+			{
+
+			Console.WriteLine("this a funtion");
+			int result = number * 10;
+
+			return result;
+
+			
+			}
+
+		public static int CrazyFunktion(int number)
+		{
+			return number * number;
+		}
+
+		public static void PrintSomethink()
+		{
+
+			Console.WriteLine("hi");
+		}
+
+		public static void PrintArray (int[] arr)
+		{
+
+			for (int i = 0; i < arr.Length; i++)
+			{
+				Console.Write("{0}, ", arr[i]);
+			}
+		}
+	}
+}
